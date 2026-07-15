de4ccd51b4d3456219dbf17026eec94ba026c00a
itsmanikandan73 <soulquiet8@gmail.com>
2026-07-15 11:40:48 +0530

    adding all the programs

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ The utilites foucs on basic File I/O(Input/Output) and standard streams.
 - ### cat ***(Concatenate)***
     - What it does: Reads files and prints their contents to the terminal.
     - *Concepts learned*: open(), read(), write(), close(), handling stdin (standard input)
+
+- ### echo
+    - What it does: Prints the arguments passed to it.
+    - *Concepts learned*: Command-line argument parsing (argc, argv), handling escape sequences (like \n)
+
+- ### false and true
+    - What it does: Literally does nothing except exit with a 0 (true) or 1 (false) status code
+    - *Concepts learned*: Process exit codes.
+    
--- my_cat.c
+++ my_cat.c
@@ -4,17 +4,17 @@
 #include <string.h>     // For strerror() to translate error codes to English
 #include <errno.h>      // For the global 'errno' variable to catch system errors
 
-#define BUFFER_SIZE 4096;
+#define BUFFER_SIZE 4096 
 
 void cat_stream(int fd) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
 
-    while ((bytes_read == read(fd, buffer, BUFFER_SIZE)) > 0) {
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
         ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
         if (bytes_written < 0) {
-            perror("Error writing to stdout")
+            perror("Error writing to stdout");
             return;
         }
     }
@@ -23,8 +23,23 @@ void cat_stream(int fd) {
     }
 }
 
-int main(int argc, char *argv){
+int main(int argc, char **argv){
 
+    if (argc == 1) {
+        cat_stream(STDIN_FILENO);  
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY);
+
+        if (fd < 0) {
+            fprintf(stderr,"my_cat: %s: %s\n", argv[i], strerror(errno));
+            continue;
+        }
+        cat_stream((fd));
+        close(fd);
+    }
 
     return 0;
 }
